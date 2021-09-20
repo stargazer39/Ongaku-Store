03685bace8b845b091a491eeb6304127487d025f
StargazeR <it20603618@my.sliit.lk>
2021-09-20 11:38:14 +0530

    SqlConnect class added

--- src/com/ongakustore/Hello.java
+++ src/com/ongakustore/Hello.java
@@ -6,14 +6,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import java.sql.Connection;
 /**
  * Servlet implementation class Hello
  */
 @WebServlet("/Hello")
 public class Hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,7 +27,9 @@ public class Hello extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		Connection conn = SqlConnect.connect();
 		response.getWriter().append("Served at: ").append(request.getContextPath()).append(" Bruh");
+		
 	}
 
 	/**
---
+++ src/com/ongakustore/SqlConnect.java
@@ -0,0 +1,44 @@
+package com.ongakustore;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SqlConnect {
+	public static Connection conn = null;
+	public static String database = "hello";
+	
+	public static Connection connect() throws FileNotFoundException {
+		try {
+			String creds[] = getCredentials();
+			String connURL = "jdbc:sqlserver://" + creds[0] + ";"
+	                + "database=" + database + ";"
+	                + "user=" + creds[1] + ";"
+	                + "password=" + creds[2] + ";"
+	                + "loginTimeout=30;";
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			conn = DriverManager.getConnection(connURL);
+			return conn;
+		}catch(Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	
+	public static String[] getCredentials() throws FileNotFoundException {
+		Path credPath = Paths.get(System.getProperty("user.home"),"credentials.txt");
+		File credentials = credPath.toFile();
+		Scanner credReader = new Scanner(credentials);
+		
+		String host = credReader.nextLine();
+		String username = credReader.nextLine();
+		String password = credReader.nextLine();
+		
+		credReader.close();
+		return new String[]{host, username, password};
+	}
+}
