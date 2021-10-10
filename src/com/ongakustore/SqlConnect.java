f115e198af271cf8e8ef23c4a980b8cd91cb38b0
StargazeR <dehemiweerakkodi2@gmail.com>
2021-10-10 08:59:59 +0530

    Added comments

--- src/com/ongakustore/SqlConnect.java
+++ src/com/ongakustore/SqlConnect.java
@@ -9,27 +9,31 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class SqlConnect {
-	public static Connection conn = null;
-	public static String database = "hello";
+	private static Connection conn = null;
+	private static String database = "Ongaku";
 	
 	public static Connection connect() throws FileNotFoundException {
 		try {
+			// Get credentials from file
 			String creds[] = getCredentials();
 			String connURL = "jdbc:sqlserver://" + creds[0] + ";"
 	                + "database=" + database + ";"
 	                + "user=" + creds[1] + ";"
 	                + "password=" + creds[2] + ";"
 	                + "loginTimeout=30;";
+			// Get connection
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			conn = DriverManager.getConnection(connURL);
 			return conn;
 		}catch(Exception e) {
-			System.out.println(e);
+			e.printStackTrace();
 			return null;
 		}
 	}
 	
+	// Utility function to get server credentials from file
 	public static String[] getCredentials() throws FileNotFoundException {
+		// Read credentials.txt from user's home dir
 		Path credPath = Paths.get(System.getProperty("user.home"),"credentials.txt");
 		File credentials = credPath.toFile();
 		Scanner credReader = new Scanner(credentials);
@@ -39,6 +43,7 @@ public class SqlConnect {
 		String password = credReader.nextLine();
 		
 		credReader.close();
+		// Return credentials in a string array
 		return new String[]{host, username, password};
 	}
 }
