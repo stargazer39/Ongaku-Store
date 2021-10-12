ccda82f87e8f6fe9a3c548669dda1e75ce238dbd
nithular <nithularatnayaker@gmail.com>
2021-10-12 20:18:48 +0530

    UserClass Fix

---
+++ src/com/ongakustore/OngakuUser.java
@@ -0,0 +1,69 @@
+package com.ongakustore;
+
+public class OngakuUser {
+	private String fname;
+	private String lname;
+	private String username;
+	private String password;
+	private String email;
+	private String gender;
+	private String country;
+	private String dob;
+	private byte[] picture; 
+	
+	public String getfname() {
+		return fname;
+	}
+	public void setfname(String fname) {
+		this.fname = fname;
+	}
+	public String getlname() {
+		return lname;
+	}
+	public void setlname(String lname) {
+		this.lname = lname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public byte[] getPicture() {
+		return picture;
+	}
+	public void setPicture(byte[] picture) {
+		this.picture = picture;
+	}
+}
