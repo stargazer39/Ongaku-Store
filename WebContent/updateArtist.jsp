cece63bdcfdcdf859538a6a58c452a1a643a1b36
StargazeR <dehemiweerakkodi2@gmail.com>
2021-10-12 17:00:17 +0530

    Update artist implementation

---
+++ WebContent/updateArtist.jsp
@@ -0,0 +1,63 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="java.sql.*, com.ongakustore.SqlConnect, com.ongakustore.Artist" %>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%
+	Artist artist = new Artist();
+
+	try{
+		String artistId = request.getParameter("artistId");
+		if(artistId == null) {
+			throw new Exception("Artist not found");
+		}
+		
+		artist.setArtistId(Integer.parseInt(artistId));
+		
+		Connection conn = SqlConnect.connect();
+		String query = "SELECT artistId, artistName, artistDescription FROM Artist WHERE artistId = ?";
+		PreparedStatement ps = conn.prepareStatement(query);
+		ps.setString(1,artistId);
+		
+		ResultSet rs = ps.executeQuery();
+		if(!rs.next()){
+			throw new Exception("Artist not found");
+		}
+		
+		artist.setArtistName(rs.getString(2));
+		artist.setArtistDescription(rs.getString(3));
+	}catch (Exception e){
+		e.printStackTrace();
+		// TODO - Redirect to error page
+		out.append(e.getMessage()).close();
+	}
+	
+%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Update</title>
+		<!-- Bootstrap -->
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+		<!-- Jquery -->
+		<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+		
+		<link rel="stylesheet" href="css/common.css">
+	</head>
+<body>
+	<div class="container">
+		<div style="width: 100px; height: 100px;">
+			<img src="/OngakuStore/getImage?type=artist&id=<%= artist.getArtistId() %>" class="img-fill">
+		</div>
+		<h1>Update <%= artist.getArtistName() %></h1>
+		<form action="updateArtist" method="POST" enctype="multipart/form-data">
+			<input type="hidden" name="artistId" value="<%= artist.getArtistId() %>">
+			<input type="text" name="artistName" placeholder="Artist" value="<%= artist.getArtistName() %>"><br>
+			<textarea name="description" placeholder="Description"><%= artist.getArtistDescription() %></textarea><br>
+			<input type="file" name="artistImage"><br><br>
+			<input type="submit" name="submit" value="Submit">
+		</form>
+	</div>
+</body>
+</html>
---
+++ src/com/ongakustore/UpdateArtistController.java
@@ -0,0 +1,83 @@
+package com.ongakustore;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import java.sql.*;
+/**
+ * Servlet implementation class UpdateArtistController
+ */
+@WebServlet("/updateArtist")
+@MultipartConfig
+public class UpdateArtistController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateArtistController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			// Check if admin authorized before sending artist info
+			boolean authorized = JSPHelper.isAuthorized(request.getSession());
+			if(!authorized){
+				 response.sendRedirect("notAuthorized.jsp");
+				 return;
+			}
+			
+			String artistId = request.getParameter("artistId");
+			String artistName = request.getParameter("artistName");
+			String description = request.getParameter("description");
+			
+			// Get artisImage stream
+			Part filePart = request.getPart("artistImage");
+			InputStream imageStream = filePart.getInputStream();
+			
+			Connection conn = SqlConnect.connect();
+			String query = "UPDATE Artist SET  artistName = ?, artistDescription = ? WHERE artistId = ?";
+			
+			PreparedStatement ps = conn.prepareStatement(query);
+			ps.setString(1,artistName);
+			ps.setString(2, description);
+			ps.setString(3, artistId);
+			
+			if(ps.executeUpdate() <= 0) {
+				throw new Exception("SQL Query error");
+			}
+			
+			if(filePart != null && filePart.getSize() != 0) {
+				updateArtistImage(artistId, imageStream);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void updateArtistImage(String id, InputStream imageStream) throws Exception {
+		Connection conn = SqlConnect.connect();
+		String query = "UPDATE Artist SET  artistImage = ? WHERE artistId = ?";
+		
+		PreparedStatement ps = conn.prepareStatement(query);
+		ps.setBinaryStream(1, imageStream);
+		
+		if(ps.executeUpdate() <= 0) {
+			throw new Exception("SQL Query error");
+		}
+	}
+
+}
